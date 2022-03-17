4ff99ad8612b4d9f0b79478cf273b2bf569d122d
RizqiAnshari <muhammadrizqia@student.telkomuniversity.ac.id>
2022-03-17 16:42:59 +0800

    first commit tpmod4

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302204024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("tesr");
+        }
+    }
+}
